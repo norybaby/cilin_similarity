f0045e386a412cbd57edcd1c799624a7ac242614
norybaby <xiuyunchen@126.com>
2017-08-31 15:54:14 +0800

    python3 test file

---
+++ py3_test_cilin.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+import ctypes
+so = ctypes.cdll.LoadLibrary   
+cilin_lib = so("./libcilin.so")
+
+cilin_lib.read_cilin('cilin.txt'.encode('UTF-8'))
+cilin_lib.similarity.restype = ctypes.c_float
+
+p_word = "人民"
+words = ["国民","群众","党群","良民","同志","成年人","市民","亲属","志愿者","先锋"]
+
+print ('compare to :',p_word)
+
+for w in words:
+    print (w,cilin_lib.similarity(p_word.encode('UTF-8'),w.encode('UTF-8')))
+
